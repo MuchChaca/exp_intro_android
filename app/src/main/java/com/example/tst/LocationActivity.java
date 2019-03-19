374da40499f5d62168bd6e1b3f2b9770105554f3
ChacaS0 <gitkraken@chks0.33mail.com>
2019-03-19 14:42:02 +0400

    Get address from longitude and latitude

--- app/src/main/java/com/example/tst/LocationActivity.java
+++ app/src/main/java/com/example/tst/LocationActivity.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.SensorManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -22,10 +24,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+import java.util.Locale;
+
 /**
  * @link https://stuff.mit.edu/afs/sipb/project/android/docs/training/basics/location/locationmanager.html
  */
-public class LocationActivity extends AppCompatActivity implements LocationListener {
+public class LocationActivity extends AppCompatActivity {
 
     public static final int PERM_LOCATION = 1 << 6;
 
@@ -34,6 +39,8 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
 
     public double longitude = 0d;
     public double latitude = 0d;
+
+    public Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +74,7 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+        geocoder = new Geocoder(this, Locale.getDefault());
 
         locationManager.requestLocationUpdates(
                 LocationManager.GPS_PROVIDER,
@@ -86,10 +94,11 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         }
     }
 
-    private void displayLocationDetails(double longitude, double latitude) {
+    private void displayLocationDetails(double longitude, double latitude, String address) {
         // Retrieve a list of location providers that have fine accuracy, no monetary cost, etc
         ( (TextView) findViewById(R.id.tv_lat) ).setText(Double.toString(latitude));
         ( (TextView) findViewById(R.id.tv_long) ).setText(Double.toString(longitude));
+        ( (TextView) findViewById(R.id.tv_adr) ).setText(address);
     }
 
     @Override
@@ -114,30 +123,7 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
     }
 
 
-    @Override
-    public void onLocationChanged(Location location) {
-        longitude = location.getLongitude();
-        latitude = location.getLatitude();
 
-        Log.d("LLLL", Double.toString(longitude) + " " + Double.toString(latitude)); // DEBUG
-
-//        displayLocationDetails(longitude, latitude);
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
 
 //    @Override
 //    protected void onResume() {
@@ -152,7 +138,20 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
             // A new location update is received.  Do something useful with it.  In this case,
             // we're sending the update to a handler which then updates the UI with the new
             // location.
-            displayLocationDetails(location.getLongitude(), location.getLatitude());
+            String address = "";
+
+//            Geocoder
+//            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            } catch (Exception ex) {
+                //
+            }
+            if(addresses.size() > 0) {
+                address = addresses.get(0).getAddressLine(0);
+            }
+            displayLocationDetails(location.getLongitude(), location.getLatitude(), address);
         }
 
         @Override
